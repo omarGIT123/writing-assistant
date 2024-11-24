f97da08e1d170385519e60602aed5300e1eb8b5d
omarGIT123 <103640522+omarGIT123@users.noreply.github.com>
2024-11-24 20:41:39 +0100

    develop

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
-from db_handler import PGhandler
 
 load_dotenv()
 app = Flask(__name__)
@@ -11,9 +10,6 @@ client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key=os.getenv("OPENAI_KEY"),
 )
-
-# Initialize db
-db_handler = PGhandler()
 
 
 @app.route("/suggestions", methods=["POST"])
@@ -24,34 +20,25 @@ def get_suggestions():
 
     text = data.get("text")
     prompt = data.get("prompt")
-    user_id = data.get("user_id")
 
     # Check for missing fields
-    if not text or not prompt or not user_id:
-        return jsonify({"error": "'text', 'prompt', and 'user_id' are required"}), 400
+    if not text or not prompt:
+        return jsonify({"error": "Both 'text' and 'prompt' are required"}), 400
 
+    # Call OpenAI's GPT-3.5 Turbo model for text generation
     try:
-        # Add input to history
-        db_handler.add_history(user_id, text)
-        db_handler.clean_history(user_id, max_records=20)
-
-        # Fetch user history for pattern recognition
-        user_history = db_handler.get_user_history(user_id, limit=20)
-        history_pattern = " ".join(user_history)
-
-        # Generate suggestions using OpenAI
+        print(text)
+        print(prompt)
         response = client.chat.completions.create(
             model="meta-llama/llama-3.2-3b-instruct:free",
             messages=[
-                {
-                    "role": "system",
-                    "content": f"{prompt}. \nAnalyze the following previous textual entries to identify key writing patterns, thought processes, habits, and unique characteristics of the user. Use these insights to tailor your suggestions for maximum relevance and personalization: {history_pattern}",
-                },
+                {"role": "system", "content": prompt},
                 {"role": "user", "content": text},
             ],
         )
+        print("\n here \n")
         suggestion = response.choices[0].message.content
-
+        print(suggestion)
         return jsonify({"suggestion": suggestion})
 
     except Exception as e:
